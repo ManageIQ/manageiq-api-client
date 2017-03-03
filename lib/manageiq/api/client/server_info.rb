adc55734c83789d9bcafa8b5a1a6074022d77dbd
Alberto Bellotti <abellott@redhat.com>
2017-03-03 13:22:36 -0500

    Enhanced ServerInfo to support the new _href entries.
    Adds the following attributes:
      - server_href
      - zone_href
      - region_href
    Updated fixture so the new entries are exercised in the rspecs.
    Updated client rspecs

--- lib/manageiq/api/client/server_info.rb
+++ lib/manageiq/api/client/server_info.rb
@@ -5,10 +5,13 @@ module ManageIQ
         attr_reader :version
         attr_reader :build
         attr_reader :appliance
+        attr_reader :server_href
+        attr_reader :zone_href
+        attr_reader :region_href
 
         def initialize(server_info)
-          @version, @build, @appliance =
-            server_info.values_at("version", "build", "appliance")
+          @version, @build, @appliance, @server_href, @zone_href, @region_href =
+            server_info.values_at("version", "build", "appliance", "server_href", "zone_href", "region_href")
         end
       end
     end
