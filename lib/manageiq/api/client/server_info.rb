16bdff8e1d785e401490bd76e37426a9ab8c46ef
Joe Rafaniello <jrafanie@gmail.com>
2026-01-07 11:20:08 -0500

    Fix tests to support new features from base entrypoint

--- lib/manageiq/api/client/server_info.rb
+++ lib/manageiq/api/client/server_info.rb
@@ -8,10 +8,14 @@ module ManageIQ
         attr_reader :server_href
         attr_reader :zone_href
         attr_reader :region_href
+        attr_reader :release
+        attr_reader :time
+        attr_reader :enterprise_href
+        attr_reader :plugins
 
         def initialize(server_info)
-          @version, @build, @appliance, @server_href, @zone_href, @region_href =
-            server_info.values_at("version", "build", "appliance", "server_href", "zone_href", "region_href")
+          @version, @build, @appliance, @server_href, @zone_href, @region_href, @release, @time, @enterprise_href, @plugins =
+            server_info.values_at("version", "build", "appliance", "server_href", "zone_href", "region_href", "release", "time", "enterprise_href", "plugins")
         end
       end
     end
